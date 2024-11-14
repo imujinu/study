1bd77c8ea48620d63ac5cac47e15719a260cdd5f
imujinu <wlsdnrhdwkd@gmail.com>
2024-11-14 23:12:29 +0900

    clone-starbucks

--- PROJECT/Clone/Strabucks/star2.js
+++ PROJECT/Clone/Strabucks/star2.js
@@ -1,10 +1,27 @@
 let btn1 = document.querySelector('.btn1')
 btn1.addEventListener('click' , function(){
-    let text1 = document.querySelector('#text')
-    text1.classList.toggle('visible')
-    let search=document.querySelector('.search')
-    search.setAttribute('width' , '18%')
-    search.setAttribute('height' , '5%')
+    let ul = document.querySelector('ul')
+    ul.classList.add('btn1transform')
+    
+    let search = document.querySelector('#search')
+    search.style.display = 'block'
 
-
+    let search1=document.querySelector('.search1')
+    search1.classList.add('btntransform')
 })
+
+let navbar = document.querySelectorAll('.header2 a')
+let hbox=document.querySelectorAll('.h-box')
+
+navbar.forEach(el1=>{
+    el1.addEventListener('mouseover', function(){
+        hbox.forEach(el=>{
+            el.classList.add('boxtransform')
+        })})
+        el1.addEventListener('mouseout', function(){
+            hbox.forEach(el=>{
+                el.classList.remove('boxtransform')
+            })}    
+
+
+)})
