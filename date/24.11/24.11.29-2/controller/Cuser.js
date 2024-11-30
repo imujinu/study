1eceb8bdcea4c657e9f6431d71c3fe7bd042362f
imujinu <wlsdnrhdwkd@gmail.com>
2024-11-30 22:13:01 +0900

    test and clone

--- date/24.11/24.11.29-2/controller/Cuser.js
+++ date/24.11/24.11.29-2/controller/Cuser.js
@@ -5,21 +5,13 @@ exports.home = (req, res) => {
 };
 
 exports.userLogin2 = (req, res) => {
-  const Info1 = user2.user.split("\n");
-  const Info2 = Info1[0].split("//");
-  const Info3 = Info1[1].split("//");
-  const Info4 = Info1[2].split("//");
-  const UserInfo = [...Info2, ...Info3, ...Info4];
-
-  if (UserInfo[0] === req.body.id && UserInfo[1] === req.body.pw) {
-    res.send({ name: UserInfo[2], success: true });
-  }
-  if (UserInfo[3] === req.body.id && UserInfo[4] === req.body.pw) {
-    res.send({ name: UserInfo[5], success: true });
-  }
-  if (UserInfo[6] === req.body.id && UserInfo[7] === req.body.pw) {
-    res.send({ name: UserInfo[8], success: true });
-  } else {
-    res.send({ succes: false });
+  const Info = user2.user.split("\n");
+  for (i = 0; i <= Info.length; i++) {
+    const userInfo = Info[i].split("//");
+    if (userInfo[0] === req.body.id && userInfo[1] === req.body.pw) {
+      res.send({ name: userInfo[2], success: true });
+    } else {
+      res.send({ success: false });
+    }
   }
 };
--- date/24.11/24.11.29-2/model/user.js
+++ date/24.11/24.11.29-2/model/user.js
@@ -1,3 +1,10 @@
-exports.user = `apple//1234//사과사과
-banana//4321//바나나나나
-cocoa//qwer1234//미떼`;
+exports.user = `grape//5678//포도포도
+peach//abcd1234//복숭복숭
+melon//9876//멜론멜론
+orange//zyxw4321//오렌오렌
+cherry//qaz0987//체리체리
+lemon//lkjh8765//레몬레몬
+pineapple//asdf5678//파인파인
+strawberry//mnbv5432//딸기딸기
+blueberry//poiuy6543//블베블베
+watermelon//hgfd5678//수박수박`;
